52db06e2590056ea4b51f205d33c629fe4f271cf
lawandepranav <lawandepranav@gmail.com>
2022-06-28 00:14:17 +0530

    commit

---
+++ src/components/Home_Page/Landing_Page/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const Navbar = () =>{
+    return(
+        <>
+        <Div>
+            <Image src="https://assets.pharmeasy.in/apothecary/images/logo_big.svg?dim=360x0"></Image>
+        </Div>
+        </>
+    )
+}
