60bd46a5afa9ccea5f9dd0edb83202f2aa92ff22
lawandepranav <lawandepranav@gmail.com>
2022-06-29 14:36:29 +0530

    commit

---
+++ pharmeasy_clone/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from "./components/Homepage/Landing_page/Navbar"
+
+function App() {
+  return (
+    <div className="App">
+  
+     <h1>Pranav</h1>
+    </div>
+  );
+}
+
+export default App;
